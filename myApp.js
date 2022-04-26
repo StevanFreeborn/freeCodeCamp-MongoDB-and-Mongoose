36d9e96a110248aac0a3ece24dd901999047ccf0
StevanFreeborn <sfreeborn7@gmail.com>
2022-04-26 14:42:51 -0500

    change;

--- myApp.js
+++ myApp.js
@@ -3,7 +3,7 @@ require('mongodb');
 
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true});
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const Schema = mongoose.Schema;
 
